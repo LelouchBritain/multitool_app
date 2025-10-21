efcba747fbced17353cdaf657250b94e2f801b3c
Bekzat <beka.omega@gmail.com>
2025-10-21 17:30:56 +0500

    Fixed deprecated "@import" from sass to "@use". Fixed error connected with environment variables being undefined in components

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    WEATHER_API_URL: process.env.WEATHER_API_URL
-  }
+    WEATHER_API_URL: process.env.WEATHER_API_URL,
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY,
+  },
 };
 
 export default nextConfig;
