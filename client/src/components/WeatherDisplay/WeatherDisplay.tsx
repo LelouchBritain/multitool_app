94b73688c621996c2478fd923c50e2346cd55c49
Bekzat <beka.omega@gmail.com>
2025-11-03 13:08:54 +0500

    Added WeatherDisplay component to display data from weather API on the front

---
+++ client/src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useWeather } from "@/context/WeatherContext";
+
+const kelToCel = (numKel: number) => {
+  return (numKel - 273.15).toFixed(2);
+};
+
+export default function WeatherDisplay() {
+  const { weatherData, unit } = useWeather();
+
+  if (!weatherData) return <p>Choose the region to see the forecast</p>;
+
+  const tempK = weatherData.main.temp;
+  const temp = unit === "kelvin" ? tempK : kelToCel(tempK);
+
+  const feelsK = weatherData.main.feels_like;
+  const feels = unit === "kelvin" ? feelsK : kelToCel(feelsK);
+
+  return (
+    <div>
+      <p>Temperature: {temp}</p>
+      <p>Feels like: {feels}</p>
+    </div>
+  );
+}
